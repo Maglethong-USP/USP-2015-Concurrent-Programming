a49d302bd2342cff6a582f32eeba4c6592afc85a
Maglethong Spirr <maglethongspirr@gmail.com>
2015-08-20 18:11:55 -0300

    Tons of comments

--- project1/lib/jacobi.h
+++ project1/lib/jacobi.h
@@ -4,9 +4,6 @@
 #ifndef __JACOBI__
 #define __JACOBI__
 
-//! Maximum number of iterations before error
-#define MAX_ITER 20
-
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -38,27 +35,114 @@ typedef struct _Jacobi
 	int iterations;
 } Jacobi;
 
-//! Struct with information for thread
+//! Struct with information for a thread
 struct _Jacobi_ThreadInfo
 {
+	//! Reference to the Jacoby struct
 	struct _Jacobi *jacobi;
+	//! Start of the lines this thread will process
 	int lineStart;
+	//! End of the lines this thread will process
 	int lineEnd;
 };
 
 
 
+//! Verify if the method is guaranteed to converge
+/*!
+	/param jacobi			The initialized Jacobi struct with its matrices properly read
+*/
 void 	Jacobi_Verify(Jacobi *jacobi);
+
+
+//! Initialize the Jacobi struct.
+/*!
+	This will not set the matrices, it will only prepare the struct.
+	Use Jacobi_Destroy() to terminate it afterwards.
+	/param jacobi			Reference to the Jacobi struct
+*/
 int 	Jacobi_Init(Jacobi *jacobi);
+
+
+//! Destroys the Jacoby struct and frees the used memory
+/*!
+	/param jacobi			The initialized Jacobi struct
+*/
 void 	Jacobi_Destroy(Jacobi *jacobi);
+
+
+//! Prints debug output to screen containing thread information
+/*!
+	/param jacobi			The initialized Jacobi struct
+*/
 void 	Jacobi_DebugThreads(Jacobi *jacobi);
+
+
+//! Prints debug output to screen containing information about both matrices and unknowns
+/*!
+	/param jacobi			The initialized Jacobi struct
+*/
 void 	Jacobi_Debug(Jacobi *jacobi);
+
+
+//! Prints debug output to screen containing information about the constants and coefficients matrices
+/*!
+	/param jacobi			The initialized Jacobi struct
+*/
 void 	Jacobi_DebugMatrix(Jacobi *jacobi);
+
+
+//! Prints debug output to screen containing information about the unknowns
+/*!
+	/param jacobi			The initialized Jacobi struct
+*/
 void 	Jacobi_DebugUnknowns(Jacobi *jacobi);
+
+
+//! Initializes the unknowns with the current values of the constants matrix
+/*!
+	This is usually a good guess to start with the method
+	/param jacobi			The initialized Jacobi struct
+*/
 void 	Jacobi_InitUnknowns(Jacobi *jacobi);
+
+
+//! Processes the constants and the coefficients matrices fur further use
+/*!
+	Changes the matrix so that the diagonal values are 1.
+	To further optimize the functions the diagonal values are set to 0 afterwards
+
+		Ex: A = | 2 4 |	b = | 2 |	-> 	A = | 1 2 |	b = | 1 |	->	A = | 0 2 |	b =	| 1 |
+				| 4 2 |		| 2 |			| 2 1 |		| 1 |			| 2 0 |		| 1 |
+
+	/param jacobi			The initialized Jacobi struct
+	/param return			0	if no error
+							> 0	if error related to threads
+*/
 int 	Jacobi_Preprocess(Jacobi *jacobi);
+
+
+//! Runs the Jacoby Method until the desired precision is reached
+/*!
+	/param jacobi			The initialized Jacobi struct
+	/param desiredPrecision	The desired precision the method will try to reach
+	/param return			0	if no error
+							> 0	if error related to threads
+*/
 int 	Jacobi_Run(Jacobi *jacobi, j_type desiredPrecision);
+
+
+//! Processes some lines of the constants and the coefficients matrices fur further use
+/*!
+	/param info				Thread information struct containing line range to process and reference to the Jacobi struct
+*/
 void 	_Jacobi_ThreadPreprocess(struct _Jacobi_ThreadInfo *info);
+
+
+//! Calculates the next iteration of the Jacobi method for some lines
+/*!
+	/param info				Thread information struct containing line range to calculate and reference to the Jacobi struct
+*/
 void 	_Jacobi_ThreadIteraction(struct _Jacobi_ThreadInfo *info);
 
 //! [Internal] Calculates one iteration of one single unknown
@@ -76,7 +160,7 @@ void 	_Jacobi_ThreadIteraction(struct _Jacobi_ThreadInfo *info);
 j_type _Jacobi_SingleIteraction(j_type **coefficients, j_type *constants, j_type *unknowns, int line, int size);
 
 
-//! [Internal] Reprocess input matrices for other functions. Only one single line is preprocessed.
+//! [Internal] Processes input matrices for other functions. Only one single line is preprocessed.
 /*!
 	Changes the matrix so that the diagonal values are 1.
 	To further optimize the functions the diagonal values are set to 0 afterwards
