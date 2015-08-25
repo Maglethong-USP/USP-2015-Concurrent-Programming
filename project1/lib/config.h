53a86292bc0a3395e45147b7a3f0988cd6ac9c56
Maglethong Spirr <maglethongspirr@gmail.com>
2015-08-25 12:43:44 -0300

    Default configuration  - Clean output  - No debug output

--- project1/lib/config.h
+++ project1/lib/config.h
@@ -2,10 +2,10 @@
 #define __CONFIG__
 
 //! Uncomment to generate debug output
-#define DEBUG
+// #define DEBUG
 
 //! Comment for clean output with only the execution time
-#define DIRTY_OUTPUT
+// #define DIRTY_OUTPUT
 
 
 #endif
